e3a64c991df5279387eae8773c28230a2d696ba7
David Anderton <dwanderton@gmail.com>
2017-12-12 00:36:41 -0500

    HW3

--- mcserver.py
+++ mcserver.py
@@ -1,4 +1,4 @@
-import ConfigParser, logging, datetime, os
+import json, ConfigParser, logging, datetime, os
 
 from flask import Flask, render_template, request
 
@@ -29,13 +29,38 @@ def home():
 @app.route("/search",methods=['POST'])
 def search_results():
     keywords = request.form['keywords']
-    now = datetime.datetime.now()
+    startdate = request.form['startdate']
+    enddate = request.form['enddate']
+    if not startdate:
+        startdate = datetime.date( 2017, 1, 1)
+    else:
+        startdate = datetime.datetime.strptime(startdate, "%Y-%m-%d")
+    if not enddate:
+        enddate = datetime.datetime.now()
+    else:
+        enddate = datetime.datetime.strptime(enddate, "%Y-%m-%d")
     results = mc.sentenceCount(keywords,
-                               solr_filter=[mc.publish_date_query( datetime.date( 2017, 1, 1), now ),
-                                            'tags_id_media:9139487'])
+                               solr_filter=[mc.publish_date_query( startdate, enddate ),
+                                            'tags_id_media:9139487'],
+                                            split=True,
+                                            split_start_date=startdate.strftime("%Y-%m-%d"),
+                                            split_end_date=enddate.strftime("%Y-%m-%d"))
+
+    data = []
+    # print(results['split'])
+    for i in results['split']:
+        temp_dict = {}
+        temp_dict['name'] = keywords
+        temp_dict['x'] = i
+        temp_dict['y'] = results['split'][i]
+        data.append(temp_dict)
+
     return render_template("search-results.html",
                            keywords=keywords,
-                           sentenceCount=results['count'] )
+                           sentenceCount=results['count'],
+                           results=json.dumps(data, sort_keys=True),
+                           startdate=startdate.strftime("%Y-%m-%d"),
+                           enddate=enddate.strftime("%Y-%m-%d"))
 
 if __name__ == "__main__":
     app.debug = True
